ea76bb2df602341ff9144ba214b7572c4626539e
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-08-20 09:12:52 +0800

    filter products

---
+++ problem-7.ts
@@ -0,0 +1,22 @@
+interface Product{
+    name:string;
+    price:number;
+   category:string;
+}
+
+
+const findProducts=(products:Product[],category:string):Product[]=>{
+
+    let categoryProduct:Product[]=products.filter((produc)=> produc.category===category)
+    return categoryProduct
+
+}
+
+
+const product=findProducts( [
+    { name: "iPhone 15", price: 90000, category: "phone" },
+    { name: "Galaxy S24", price: 85000, category: "phone" },
+    { name: "MacBook Air", price: 120000, category: "laptop" },
+    { name: "Dell XPS", price: 110000, category: "laptop" }
+])
+console.log(product)
